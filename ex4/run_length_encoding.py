9d8a32016737a69feeb92f17ca204f7931215700
MarVidalFolch <74255454+MarVidalFolch@users.noreply.github.com>
2020-11-11 22:35:10 +0100

    Update run_length_encoding.py

--- ex4/run_length_encoding.py
+++ ex4/run_length_encoding.py
@@ -3,23 +3,23 @@
 
 def run_length_encode():
 
-    data = input("Enter series to encode: ")
-    encoding = ''
-    prev_char = ''
-    count = 1
+    data = input("Enter series to encode: ")  # ask to the user to input the series
+    encoding = ''  # init encoding string that will be the return of the function 
+    prev_char = ''  # init prev_char string used to compare characters
+    count = 1  # init the counter to 1
 
-    for char in data:
+    for char in data:  # start a loop through the input data
         if char != prev_char:  # if the previous and current characters don't match:
-            if prev_char:
+            if prev_char:  # if we are not in the first position of the data string
                 encoding += str(count) + prev_char  # add the count and the character to our decoding
             count = 1
-            prev_char = char
+            prev_char = char  # if we are in the fisrt position of the data string, set char as prev_char
         else:  # if the characters match, increment the counter
             count += 1
     else:  # finish off the encoding
-        encoding += str(count) + prev_char
-        return encoding
+        encoding += str(count) + prev_char  # append to the encoding string the encoded char
+        return encoding  
 
-
+# call the function an print its result
 encoded_seq = run_length_encode()
 print('Encoded sequence: ', encoded_seq)
